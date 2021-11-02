400142d2300567df66ebdf6efc7acbacbed58e61
Brunkki <bruno.zagorac@icloud.com>
2021-11-02 19:34:46 +0200

    SnakeBoard

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+import Point from './Point';
+import SnakeBoard from "./SnakeBoard";
+
+const App = () => {
+  const [ points, setPoints ] = useState(0)
+  return (
+    <div className="app">
+      <header className="header"> Matopeli </header>
+      <Point pisteet={points} />
+      <SnakeBoard points={points} setPoints={setPoints} />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Point.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Point.css";
+
+const Points = ({pisteet}) => {
+    console.log();
+    return <div className="point"> {pisteet} pistettä </div>
+}
+
+export default Points
